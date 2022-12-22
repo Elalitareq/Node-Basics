81c7ebe1860c6d7cc8d048d2baa2c4fa84fee089
Tareq El-Ali <elalitareq@gmail.com>
2022-12-22 22:01:26 +0200

    edit

--- tasks.js
+++ tasks.js
@@ -1,63 +1,57 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------");
 }
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n'|| text === 'exit\n') {
+  if (text === "quit\n" || text === "exit\n") {
     quit();
-  }
-  else if(text.startsWith("hello")){
+  } else if (text.startsWith("hello")) {
     hello(text);
-  }
-  else if(text==='help\n'){
-    help()
-  }
-  else if(text==='list\n'){
-    list()
-  }
-  else if(text.startsWith('add')){
-    addTask(text)
-  }
-  else if(text.startsWith('remove')){
-    remove(text)
-  }
-  else{
+  } else if (text === "help\n") {
+    help();
+  } else if (text === "list\n") {
+    list();
+  } else if (text.startsWith("add")) {
+    addTask(text);
+  } else if (text.startsWith("remove")) {
+    remove(text);
+  } else if (text.startsWith("edit")) {
+    edit(text);
+  } else {
     unknownCommand(text);
   }
 }
 
-let arrayList=[ ]
+let arrayList = [];
 
 /**
  * prints "unknown command"
@@ -66,18 +60,17 @@ let arrayList=[ ]
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(anything){
-  console.log(`${anything.trim()}!`)
+function hello(anything) {
+  console.log(`${anything.trim()}!`);
 }
 
 /**
@@ -85,9 +78,9 @@ function hello(anything){
  *
  * @returns {void}
  */
-function help(){
+function help() {
   console.log(
-`hello /text/: Returns hello /text/!
+    `hello /text/: Returns hello /text/!
               syntex: hello /text/
 
 help        : Reutrns commands user can use
@@ -105,7 +98,8 @@ remove      : Removes a take from tast-list by its number
 exit or quit: Quits the application
               syntex: quit
                       exit
-`)
+`
+  );
 }
 
 /**
@@ -113,56 +107,74 @@ exit or quit: Quits the application
  *
  * @returns {void}
  */
-function list(){  
-  arrayList.map((element,index)=>{
-    console.log(`${index+1}-${element}`)
-})
+function list() {
+  arrayList.map((element, index) => {
+    console.log(`${index + 1}-${element}`);
+  });
 }
 /**
  * adds items to the list
  *
  * @returns {void}
  */
-function addTask(task){ 
-  if(task==="add\n") {
+function addTask(task) {
+  if (task === "add\n") {
     console.log(
-`the correct syntex for add is:
-                                add /task/`)
+      `the correct syntex for add is:
+                                add /task/`
+    );
+  } else {
+    arrayList.push(task.replace("add ", "").trim());
   }
-  else{
-  arrayList.push(task.replace("add ","").trim())}
 }
 /**
  * removes a task from task-list
  *
  * @returns {void}
  */
-function remove(taskNum){
-  if(taskNum==="remove\n") {
+function remove(taskNum) {
+  if (taskNum === "remove\n") {
     console.log(
-`the correct syntex for remove is:
-                                  remove /task-number/`)
-  }
-  else{
-  let index=taskNum.replace("remove ","").trim()-1
-  if (arrayList[index]!==undefined){
-  arrayList.splice(index,1)
-  }
-  else {
-    console.log(`No task with number ${index+1}`);
+      `the correct syntex for remove is:
+                                  remove /task-number/`
+    );
+  } else {
+    let index = taskNum.replace("remove ", "").trim() - 1;
+    arrayList[index] !== undefined
+      ? arrayList.splice(index, 1)
+      : console.log(`No task with number ${index + 1}`);
   }
 }
+/**
+ * edits tasks in task-list
+ *
+ * @returns {void}
+ */
+function edit(taskNum) {
+  if (taskNum === "edit\n") {
+    console.log(
+      `the correct syntex for edit is:
+                                to edit a specific task-number  : edit /task-number/ /new tast/
+                                to edit the last task           : edit /new task/`
+    );
+  } else {
+    let index = taskNum.split(" ")[1] - 1;
+    !parseInt(index)
+      ? (arrayList[arrayList.length - 1] = taskNum.replace("edit ", "").trim())
+      : arrayList[index] !== undefined
+      ? (arrayList[index] = taskNum.replace(`edit ${index + 1}`, "").trim())
+      : console.log(`No task with number ${index + 1}`);
+  }
 }
 /**
  * Exits the application
  *
  * @returns {void}
  */
-function quit(){
-  console.log('Quitting now, goodbye!')
+function quit() {
+  console.log("Quitting now, goodbye!");
   process.exit();
 }
 
-
 // The following line starts the application
-startApp("Tareq El-Ali")
+startApp("Tareq El-Ali");
